bd0a63c8f80922ca6fe74b47011bc3a10828cf70
artem <artemsa223@gmail.com>
2024-07-14 23:11:10 +0100

    init commit

---
+++ src/main/java/cu/theatre/CuTheatreApplication.java
@@ -0,0 +1,13 @@
+package cu.theatre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuTheatreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuTheatreApplication.class, args);
+    }
+
+}
